6bd12953f47126a8ac624e4e3d193b343a2700b3
saurabhkoshatwar <35650601+saurabhkoshatwar@users.noreply.github.com>
2018-09-25 10:12:17 +0530

    serial merge Sort

---
+++ openmp/sorting/merge_sort.cpp
@@ -0,0 +1,201 @@
+#include<iostream>
+#include<omp.h>
+double total_time_serial,total_time_parallel;
+using namespace std;
+
+void merge(int arr[],int l,int m,int r)
+{
+	//SIZE
+	int size1 = m - l +1; // m - (l-1) ---removing prev part upto (l-1)	
+	int size2 = r - m    ;// 2nd array from m+1 to r --- removing prev part upto m
+	
+	//Get Part
+	int L[size1],R[size2];
+	for(int i=0;i<size1;i++)
+		L[i] = arr[l+i];  // get arr part from l to size1
+	for(int j=0;j<size2;j++)
+		R[j] = arr[m + 1 + j]; // get arr part from m+1 to size2
+
+	//COMBINE
+	i=0;   //ptr to L[]
+	j=0;   //ptr to R[]
+	int k=l;
+
+			//combine L[] and R[] into arr in place l to r with sort
+	while( i<size1 && j<size2)
+	{
+		if(L[i]<=R[j])	
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < size1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < size2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 		
+		 
+}
+
+
+void mergesort(int arr[],int l,int r)
+{
+	if(l < r)
+	{	
+		// Same as (l+r)/2, but avoids overflow for 
+        // large l and r
+		int m = l + (r-l)/2;
+		
+		mergesort(arr,l,m);
+		mergesort(arr,m+1,r);
+
+		merge(arr,l,m,r); 
+	}	
+}
+int main()  
+{
+    int n = 10;
+    int * arr = new int[n];
+	int * arr2 = new int[n];
+    double start, end;
+    int num_thread;
+    
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = rand(); 
+	    	arr2[i] = arr[i];   
+        }
+        
+	start = omp_get_wtime(); 
+     	
+	//Serial Merge Sort
+	
+	
+
+	end = omp_get_wtime();
+
+    total_time_serial = end-start;
+    
+	cout<<"Bubble Sort total_time_serial- "<<bbs_total_time_serial<<endl;
+
+	start = omp_get_wtime(); 
+     	
+	//Odd Even Sort
+	bool isSorted = false; // Initially array is unsorted
+ 
+    while (!isSorted)
+    {
+        isSorted = true;
+ 
+        // Perform Bubble sort on odd indexed element
+        for (int i=1; i<=n-2; i=i+2)
+        {
+            if (arr2[i] > arr2[i+1])
+             {
+                swap(arr2[i], arr2[i+1]);
+                isSorted = false;
+              }
+        }
+ 
+        // Perform Bubble sort on even indexed element
+        for (int i=0; i<=n-2; i=i+2)
+        {
+            if (arr2[i] > arr2[i+1])
+            {
+                swap(arr2[i], arr2[i+1]);
+                isSorted = false;
+            }
+        }
+    }
+
+	end = omp_get_wtime();
+
+    oes_total_time_serial = end-start;
+    
+	cout<<"Odd even Sort total_time_serial- "<<oes_total_time_serial<<endl;
+
+    for(int k=1;k<=4;k++)
+    {
+            omp_set_num_threads(k);
+
+            start = omp_get_wtime();
+		
+            #pragma omp parallel 
+            {
+                num_thread = omp_get_num_threads();
+			}
+
+		bool isSorted = false;
+		while (!isSorted)
+   		{
+        	isSorted = true;
+            #pragma omp parallel 
+            {
+                
+                #pragma omp for
+                for (int i=1; i<=n-2; i=i+2)
+       			 {
+				     if (arr3[i] > arr3[i+1])
+				     {
+				        swap(arr3[i], arr3[i+1]);
+						#pragma omp critical
+				        isSorted = false;
+				     }
+        		 }
+				//#pragma omp for nowait    // odd even parallel (wrong way/correct ans/more loops)
+				#pragma omp for 
+				for (int i=0; i<=n-2; i=i+2)
+				{
+				    if (arr3[i] > arr3[i+1])
+				    {
+				        swap(arr3[i], arr3[i+1]);
+						#pragma omp critical
+				        isSorted = false;
+				    }
+				}
+            }
+		}
+
+            end = omp_get_wtime();
+            total_time_parallel = end-start;
+            cout<<"total_time_parallel with threads("<<num_thread<<")- "<<total_time_parallel<<endl;
+    }
+
+	
+	//cout<<"Bubble sort serial\t"<<"Odd Even Sort Serial\t"<<"Odd Even Sort Parallel"<<endl;	 
+	bool flag=true;	
+	for(int i=0;i<n;i++)
+     {
+         //cout<<arr[i]<<"\t\t"<<arr2[i]<<"\t\t"<<arr3[i]<<endl;
+		 if(arr[i]!=arr2[i] && arr3[i]!=arr2[i])
+		 flag=false;	
+     }
+
+	if(flag)
+	cout<<"\ncorrect\n";
+	else
+	cout<<"\nIncorrect\n";
+
+     return 0;
+
+}
